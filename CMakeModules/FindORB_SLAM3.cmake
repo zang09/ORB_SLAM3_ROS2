5bcf1ac79ce6884efa426961d444756da705a4da
Fran-FC <frafolcm@gmail.com>
2023-06-27 14:45:53 +0000

    Added opencv lib path

--- CMakeModules/FindORB_SLAM3.cmake
+++ CMakeModules/FindORB_SLAM3.cmake
@@ -5,7 +5,7 @@
 #
 # To help the search ORB_SLAM3_ROOT_DIR environment variable as the path to ORB_SLAM3 root folder
 #  e.g. `set( ORB_SLAM3_ROOT_DIR=~/ORB_SLAM3) `
-set(ORB_SLAM3_ROOT_DIR "~/Install/ORB_SLAM/ORB_SLAM3")
+set(ORB_SLAM3_ROOT_DIR "~/Coding/ORB_SLAM3")
 
 # message(${ORB_SLAM3_ROOT_DIR})
 # message(${ORB_SLAM3_ROOT_DIR}/include)
